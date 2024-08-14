8c2b1a3b5b8ab986d585af71943f0f049b9e01f6
teradakyouhei <fbosaitera@gmail.com>
2024-08-14 19:44:06 +0900

    課題５提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 変数の宣言
+let UserBodyweight;
+
+let UserHeight;
+
+let UserBodymassindex;
+
+// 値の代入
+UserBodyweight = 68
+
+UserHeight = 1.7
+
+UserBodymassindex = UserBodyweight / (UserHeight * UserHeight)
+
+// コンソールへの出力
+console.log(UserBodymassindex)
