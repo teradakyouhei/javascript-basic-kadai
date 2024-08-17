d318905a8ee545c02242b493cbfef0236aadc400
teradakyouhei <fbosaitera@gmail.com>
2024-08-17 15:00:32 +0900

    7章課題提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = 5;
+
+if (num % 3 === 0 && num % 5 !== 0) {
+    console.log("3の倍数です")
+}
+
+if (num % 5 === 0 && num % 3 !== 0 ) {
+    console.log("5の倍数です")
+}
+
+if (num % 3 === 0 && num % 5 === 0 ) {
+    console.log("3と5の倍数です")
+}
+
+if (num % 3 !== 0 && num % 5 !== 0) {
+    console.log(num)
+}
+
